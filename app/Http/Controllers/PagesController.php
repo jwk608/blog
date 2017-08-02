4b01e2ed19936bddc894d7c906d21201ec8a295e
jayden <joongwonkim608@gmail.com>
2017-08-02 13:14:04 -0400

    partials with blade

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,11 +9,20 @@ class PagesController extends Controller {
 	}
 
 	public function getAbout(){
-		return view('pages.about');
+		$first = 'Jayden';
+		$last = 'Kim';
+		$email = 'jayden@gmail.com';
+
+		$fullname = $first . " " . $last;
+
+		$data = [];
+		$data['email'] = $email;
+		$data['fullname'] = $fullname;
+		return view('pages.about')->withData($data);
 	}
 
 	public function getContact(){
 		return view('pages.contact');
 	}
 
-}
+} 
