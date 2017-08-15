1200860a5ce78ef1e4c4219dc03d4eebf995685d
jayden <joongwonkim608@gmail.com>
2017-08-15 14:40:58 -0400

    Edit and Delete

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,9 +3,35 @@
 @section('title', '| View Post')
 
 @section('content')
-	
-	<h1>{{ $post->title }}</h1>
+	<div class="row">
+		<div class="col-md-8">
+			<h1>{{ $post->title }}</h1>
 
-	<p class="lead"> {{ $post->body }}</p>
+			<p class="lead"> {{ $post->body }}</p>
+		</div>
 
+		<div class="col-md-4">
+			<div class="well">
+				<dl class="dl-horizontal">
+					<dt>Create At:</dt>
+					<dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+				</dl>
+
+				<dl class="dl-horizontal">
+					<dt>Last Updated:</dt>
+					<dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+				</dl>
+				<hr>
+				<div class="row">
+					<div class="col-sm-6">
+ 						<a href="{{  route('posts.edit', $post->id, 'Edit')  }}" class="btn btn-primary btn-block"> Edit</a>﻿						
+					</div>
+					<div class="col-sm-6">
+ 						<a href="{{  route('posts.destroy', $post->id, 'Delete')  }}" class="btn btn-danger btn-block"> Delete</a>﻿						
+					</div>
+										
+				</div>				
+			</div>
+		</div>
+	</div>
 @endsection
