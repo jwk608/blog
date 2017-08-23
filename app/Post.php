ece8dcbf916062fd84d2e6bfff8b97eb29f667a9
jayden <joongwonkim608@gmail.com>
2017-08-23 12:44:22 -0400

    Tag model and tables

--- app/Post.php
+++ app/Post.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-	public function Category()
+	public function category()
 	{
     	return $this->belongsTo('App\Category');
+	}
+
+	public function tags(){
+		return $this->belongsToMany('App\Tag');
 	}
 }
