a939c507338706927c154ced2f9caab26bda579e
Joong <joongwonkim608@gmail.com>
2020-07-27 18:05:39 -0400

    add collision handle on socialate

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -6,28 +6,35 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use App\User;
+use Illuminate\Support\Facades\Redirect;
 
 class SocialAuthController extends Controller
 {
     public function redirectToProvider($provider)
     {
-    	return Socialite::driver($provider)->redirect();
+        return Socialite::driver($provider)->redirect();
     }
- 
+
     public function handleProviderCallback($provider)
     {
-    	$user = Socialite::driver($provider)->user();
+        $user = Socialite::driver($provider)->user();
 
         $authUser=User::firstOrNew(['provider_id'=>$user->id]);
 
-        $authUser->name=$user->name;
-        $authUser->email=$user->email;
-        $authUser->provider=$provider;
+        //if provider does not match
+        if($authUser->provider != $provider) {
+            return Redirect::back()->with('status', 'You are already are a memeber! Please sign in using original method!');
 
-        $authUser->save();
-
-        auth()->login($authUser);
-
+        }else {
+            $authUser->name=$user->name;
+            $authUser->email=$user->email;
+            $authUser->provider=$provider;
+    
+            $authUser->save();
+    
+            auth()->login($authUser);
+        }
+       
         return redirect('/');
 
     }
